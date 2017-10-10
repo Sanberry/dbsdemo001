cefe16ccfc1ad7d59471e18212607f8eba1fa3db
Sanberry <32483996+Sanberry@users.noreply.github.com>
2017-10-10 13:29:21 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,4 @@
 echo "<h1>Hello World Version 1.0</h1>";
 echo "<p>Hello World from Heroku</p>";
 echo "<p>This is a new paragraph</p>";
-echo "<p><a> href="peopleapi.php" Link to People API</a></p>";
+echo "<p><a href="peopleapi.php"> Link to People API</a></p>";
