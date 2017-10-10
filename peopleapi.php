3e4f6f8a998bf1161e115936f74625cb8617a302
Sanberry <32483996+Sanberry@users.noreply.github.com>
2017-10-10 13:24:12 +0100

    Update peopleapi.php

--- peopleapi.php
+++ peopleapi.php
@@ -1,1 +1,36 @@
+<?php
+/*Declare a class to a contain a person record */
 
+class Person {
+  
+    var $personID;
+    var $firstName;
+    var $lastName;
+  
+}
+
+/*Declare your variables */
+  $items = array();
+
+/*Populate your array with a list of dummy records */
+
+  for($i = 0; $i < 10; $i++) {
+      $personRecord = new Person();
+      $personRecord->personID = $i;
+      $personRecord->firstName = "Name $i";
+      $personRecord->lastName = "LastName $i";
+      $items[$i] = $personRecord;
+  }
+/*Set the header type for output */
+  
+  header('Content-Type: application/json');
+
+/* Create a variable to hold your JSON data */
+  
+  $jsonOutput = json_encode($items);
+
+/*Output the JSON data */
+
+  echo $jsonOutput;
+
+?>
